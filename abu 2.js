c1d5d84a85e87435d0250efd8454c213cf1d9303
luizaohmr99@gmail.com <luizaohmr99gmail.com>
2026-03-23 20:35:10 -0300

    abu trabalhos

---
+++ abu 2.js
@@ -0,0 +1,6 @@
+let nota1 = 10
+let nota2 = 2
+console.log(nota1 > nota2)
+console.log(nota1 < nota1)
+console.log(nota1 != nota2)
+console.log(nota1 == nota2)
---
+++ abu.js
@@ -0,0 +1,4 @@
+let altura = 10
+let largura = 20
+let area = (altura * largura)
+console.log(area)
