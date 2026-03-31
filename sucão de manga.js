3b8e94bc95d2d8ba4f38650b7ba6acc2a1643ee7
luizaohmr99@gmail.com <luizaohmr99gmail.com>
2026-03-30 21:23:34 -0300

    suco de mangão

---
+++ sucão de manga.js
@@ -0,0 +1,14 @@
+let opcao = 4
+switch (opcao) {
+case 1:
+console.log("Você escolheu cachaça")
+break
+case 2:
+console.log("Você escolheu conhaque com coca-cola")
+break
+case 3:
+console.log("Você escolheu chimas")
+break
+case 4:
+console.log("você escolheu sucão de manga")
+}
